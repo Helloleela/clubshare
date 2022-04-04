53ee17c02f92c258c055a8ff1e143bc9060ec71e
leeladewa <leeladhardewangan9@gmail.com>
2022-04-04 11:32:25 +0530

    my third commit

--- clubing/events/views.py
+++ clubing/events/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'events/home.html',{})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name="Leela"
+    month = month.title()
+    month_number = list(calendar.month_name).index(month)
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    curr_year = datetime.now().year
+    return render(request, 'events/home.html',
+    {    "name":name,
+         "year":year,
+         "month": month,
+         "cal" : cal,
+         "current_year":curr_year,
+          
+    })
